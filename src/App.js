31e830d229704f7eb39e719cbddad12e45d8748e
Kerry Imai <kc815hk@hotmail.com>
2017-03-21 00:08:57 -0400

    Add sidebar component

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Sidebar from './components/Sidebar';
 
+
 import './App.css';
 
 const App = () => (
   <BrowserRouter>
     <div>
 
-        <Sidebar />
-
+      <Sidebar>
         <Route exact path="/dashboard" component={Dashboard} />
+      </Sidebar>
     </div>
   </BrowserRouter>
 );
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
-
+import SidebarComponent from 'react-sidebar';
+import './index.css';
 
 
 
 class Sidebar extends Component {
+  state = {sidebarOpen: true};
+
+
+  onSetSidebarOpen = (open) => {
+    this.setState({sidebarOpen: open})
+  };
+
+  menuButtonClick(ev) {
+   ev.preventDefault();
+   this.onSetSidebarOpen(!this.state.open);
+ };
+
   render() {
+    const sidebarContent = (
+      <div className="sidebar">
+        <b>Sidebar content</b>
+        <h4>Login</h4>
+        <h4>Home</h4>
+        <h4>About us</h4>
+        <h4>contact us</h4>
+      </div>
+    );
     return (
-      <h1>Sidebar</h1>
+      <SidebarComponent
+        sidebar={sidebarContent}
+        open={this.state.sidebarOpen}
+        onSetOpen={this.onSetSidebarOpen}
+      >
+        <a onClick={this.menuButtonClick.bind(this)} href="#">=</a>
+        {this.props.children}
+      </SidebarComponent>
     );
   }
 }
